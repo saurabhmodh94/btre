90f07812fd3ecc387ce3e4c3a13efc9ae42118f4
Saurabh Modh <saurabhmodh94@gmail.com>
2019-01-13 23:35:33 +0530

    Send Email

--- btre/settings.py
+++ btre/settings.py
@@ -146,3 +146,16 @@ MESSAGE_TAGS = {
     # messages.INFO: '',
     # 50: 'critical',
 }
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+#Must generate specific password for your app in [gmail settings][1]
+EMAIL_HOST_PASSWORD = ''
+
+#This did the trick
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
